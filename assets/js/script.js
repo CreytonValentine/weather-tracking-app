d4f9bb57e2892a307a19d285e8bbcf88e47db0fd
CreyValentine <valentine.crey95@gmail.com>
2022-08-24 22:36:39 -0500

    displayed input value

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,15 @@ var cityNameEl = document.querySelector("#city-name");
 var nameContainerEl = document.querySelector("#city-weather");
 var enteredCityName = document.querySelector("#city-name-search");
 
-var displayCity = function(city, searchCity) {
+var displayCity = function(city, cityName) {
+    nameContainerEl.textContent = "";
+    enteredCityName.textContent = cityName;
     console.log(city);
-    console.log(searchCity)
+    console.log(cityName);
+
+    for (var i = 0; i < city.length; i++) {
+        var 
+    }
 }
 
 //create form handler 
@@ -25,16 +31,16 @@ var formSubmit = function(event) {
 }
 
 //create variable with website url and website api request
-var weatherInfo = function() {
+var weatherInfo = function(city) {
     var url = "https://api.openweathermap.org/data/2.5/weather?q=San-Antonio&appid=42b9bf4f152c90dbffc98bec0d242a6a";
     
     console.log(url);
 
-   //fetch(url).then(function(response) {
-        //response.json().then(function(city) {
-            //displayCity(city, temp);
-        //});
-    //});
+   fetch(url).then(function(response) {
+        response.json().then(function(data) {
+            displayCity(data, city);
+        });
+    });
 };
 
 //fetch("https://api.openweathermap.org/data/2.5/weather?q=San-Antonio&appid=42b9bf4f152c90dbffc98bec0d242a6a").then(function(response) {
